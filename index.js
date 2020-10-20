475a993ad387dee0dd8d86d30045102a8b9dbe52
Juan Bonami <Juanbonami@gmail.com>
2020-10-20 14:15:33 -0500

    converting the value in local torage from a string to an integer

--- index.js
+++ index.js
@@ -45,6 +45,11 @@ for (let i=0; i<carts.length; i++) {
     carts[i].addEventListener('click', () => {
         let productNumbers = localStorage.getItem('cartNumbers');
         console.log(productNumbers);
+        console.log(typeof productNumbers);
+
+        productNumbers = parseInt(productNumbers);
+        console.log(typeof productNumbers);
+
         localStorage.setItem('cartNumbers', 1);
     })
 }
