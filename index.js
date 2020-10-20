637b51b3f1faf960933e2b2400f8929dc49242ab
Juan Bonami <Juanbonami@gmail.com>
2020-10-20 12:30:26 -0500

    using local storage to keep track of items put in cart

--- index.js
+++ index.js
@@ -43,6 +43,6 @@ let carts = document.querySelectorAll('.add-cart');
 
 for (let i=0; i<carts.length; i++) {
     carts[i].addEventListener('click', () => {
-        console.log("I'm working")
+        localStorage.setItem('cartNumbers', 1);
     })
 }
