0df159968d3c65856f9c45ae17fe326ab456518d
Juan Bonami <Juanbonami@gmail.com>
2020-10-25 23:15:19 -0500

    created a function to open cart and slide it from top

--- index.js
+++ index.js
@@ -127,3 +127,8 @@ function totalCost(product) {
 }
 
 onLoadCartNumbers();
+
+function openCart() {
+    document.getElementById('cart').style.height="300px";
+    document.getElementById('mainbox').style.marginTop="300px";
+}
