60af45a111538dde886be9fc99da3a949ca863dc
juan <juanbonami@gmail.com>
2021-03-11 10:52:25 -0600

    added update cart function, gets the quantity of the cart and times it by the price to get the total price, also added and event listener to listen if the body has been loaded in order to run the script while or after the body has loaded

--- index.js
+++ index.js
@@ -39,18 +39,44 @@ faders.forEach(fader => {
 })
 
 
-
-let removeCartItemButton = document.getElementsByClassName('remove-button');
-console.log(removeCartItemButton);
-for (let i = 0; i < removeCartItemButton.length; i++) {
-    let button = removeCartItemButton[i]
-    button.addEventListener('click', removeCartItem)
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
 }
+
+
+function ready() {
+    let removeCartItemButton = document.getElementsByClassName('remove-button');
+    console.log(removeCartItemButton);
+    for (let i = 0; i < removeCartItemButton.length; i++) {
+        let button = removeCartItemButton[i]
+        button.addEventListener('click', removeCartItem)
+    }
+}
+
 
 function removeCartItem(event) {
     let buttonClicked = event.target
     buttonClicked.parentElement.parentElement.remove()
+    updateCartTotal()
 }
+
+function updateCartTotal() {
+    let cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    let cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    let total = 0
+    for (var i = 0; i < cartRows.length; i++) {
+        let cartRow = cartRows[i]
+        let priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        let quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        let price = parseFloat(priceElement.innerText.replace('$', ''))
+        let quantity = quantityElement.value
+        total = total + (price * quantity)
+    }
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+}
+
 
 // let carts = document.querySelectorAll('.add-cart');
 
